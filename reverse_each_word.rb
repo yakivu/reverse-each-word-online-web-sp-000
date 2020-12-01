2aca90a40f6532a5b102b6df551d8d589355921b
Adam Walker <adamw207@yahoo.com>
2020-12-01 18:50:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
-reverse_each_word("Hello there, and how are you?")
-puts("Hello there, and how are you?").reverse 
-.each 
-.collect 
+#reverse_each_word("Hello there, and how are you?")
+#puts("Hello there, and how are you?").reverse 
+#.each 
+#.collect 
 
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
